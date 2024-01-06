98449af14afb3d33583dddb32153c3db49867dd0
unknown <jeffraj00000@gmail.com>
2024-01-06 21:25:41 +0530

    sumoftwono

---
+++ sumoftwono.py
@@ -0,0 +1,7 @@
+Value1=int(input("Enter a value1 : "))
+Value2=int(input("Entet a value2 : "))
+
+Output=Value1 + Value2
+print("Result is : ",Output )
+
+print("Result in print statement : ",Value1+Value2)
